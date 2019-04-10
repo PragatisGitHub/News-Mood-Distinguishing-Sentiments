5dba45ed457f5b89b6fb4bc256d191a324891b05
Pragati Mehta <pragati.soni.mehta.career@gmail.com>
2019-04-10 15:54:53 -0400

    Adding final modules

---
+++ config.py
@@ -0,0 +1,12 @@
+# Twitter API Keys
+#Generated on 21st June 2018
+consumer_key = "aNLSAdW7yInFcn2K2ZyiQPcot"
+consumer_secret = "yDRFdvSvbCxLCUyBMbuqgkmidGiuDLPnSciG6WI2NGVzjrShsF"
+access_token = "1009876849122521089-5hIMxvbSQSI1I1wgiYmKysWrMe48n3"
+access_token_secret = "jcWvCTMUalCk2yA5Ih7ZoZwVZlbZwYx5BRz1s7P4CkkS0"
+
+# #Generated on Jun 23 2018
+# consumer_key = "n9WXASBiuLis9IxX0KE6VqWLN"
+# consumer_secret = "FqJmd8cCiFhX4tKr91xAJoBL0s5xxp9lmv3czEW84mT3jsLCj7"
+# access_token = "1009876849122521089-Sok0ZuMt7EYOLucBhgyDefaQlYJkXX"
+# access_token_secret = "IDt79lugJ9rsqa9n9Oly38Xr4rvFlrSzRWb0quPxJNnUg"
